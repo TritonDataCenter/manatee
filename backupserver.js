55618cc824b76c2e93c38e165161187014493197
Yunong Xiao <yunong@joyent.com>
2012-12-04 02:03:36 +0000

    MANTA-713 use posix-getopt for backupserver.js

--- backupserver.js
+++ backupserver.js
@@ -1,53 +1,101 @@
 // Copyright (c) 2012, Joyent, Inc. All rights reserved.
+var assert = require('assert-plus');
 var bunyan = require('bunyan');
+var extend = require('xtend');
 var fs = require('fs');
-var optimist = require('optimist');
+var getopt = require('posix-getopt');
 var BackupServer = require('./lib/backupServer');
 var BackupSender = require('./lib/backupSender');
 
-///--- Mainline
-
-var ARGV = optimist.options({
-        'd': {
-                alias: 'debug',
-                describe: 'debug level'
-        },
-        'f': {
-                alias: 'file',
-                demand: true,
-                describe: 'configuration file'
-        }
-}).argv;
-
-var CFG;
+/**
+ * globals
+ */
+var NAME = 'manatee-backupserver';
 
 var LOG = bunyan.createLogger({
-        level: ARGV.d ? (ARGV.d > 1 ? 'trace' : 'debug') : 'info',
-        name: 'backupSender',
+        level: (process.env.LOG_LEVEL || 'info'),
+        name: NAME,
         serializers: {
                 err: bunyan.stdSerializers.err
         },
-        src: ARGV.d ? true : false
+        // always turn source to true, manatee isn't in the data path
+        src: true
 });
 
-function readConfig() {
-        if (!CFG) {
-                CFG = JSON.parse(fs.readFileSync(ARGV.f, 'utf8'));
-                LOG.info({config: CFG, file: ARGV.f}, 'Configuration loaded');
+var LOG_LEVEL_OVERRIDE = false;
+
+/**
+ * private functions
+ */
+function parseOptions() {
+        var option;
+        var opts = {};
+        var parser = new getopt.BasicParser('vf:(file)', process.argv);
+
+        while ((option = parser.getopt()) !== undefined) {
+                switch (option.option) {
+                case 'f':
+                        opts.file = option.optarg;
+                        break;
+
+                case 'v':
+                        // Allows us to set -vvv -> this little hackery
+                        // just ensures that we're never < TRACE
+                        LOG_LEVEL_OVERRIDE = true;
+                        LOG.level(Math.max(bunyan.TRACE, (LOG.level() - 10)));
+                        if (LOG.level() <= bunyan.DEBUG)
+                                LOG = LOG.child({src: true});
+                        break;
+
+                default:
+                        process.exit(1);
+                        break;
+                }
         }
 
-        return (CFG);
+        return (opts);
 }
 
-var cfg = readConfig();
-cfg.backupServerCfg.log = LOG;
-cfg.backupSenderCfg.log = LOG;
+function readConfig(options) {
+        assert.object(options);
 
-var server = new BackupServer(cfg.backupServerCfg);
+        var cfg;
+
+        try {
+                cfg = JSON.parse(fs.readFileSync(options.file, 'utf8'));
+        } catch (e) {
+                LOG.fatal({
+                        err: e,
+                        file: options.file
+                }, 'Unable to read/parse configuration file');
+                process.exit(1);
+        }
+
+        return (extend({}, cfg, options));
+}
+
+///--- Mainline
+var _config;
+var _options = parseOptions();
+
+LOG.debug({options: _options}, 'command line options parsed');
+_config = readConfig(_options);
+LOG.debug({config: _config}, 'configuration loaded');
+
+if (_config.logLevel && !LOG_LEVEL_OVERRIDE) {
+        if (bunyan.resolveLevel(_config.logLevel)) {
+                LOG.level(_config.logLevel);
+        }
+}
+
+_config.backupServerCfg.log = LOG;
+_config.backupSenderCfg.log = LOG;
+
+var server = new BackupServer(_config.backupServerCfg);
 
 // server and sender share the same queue
-cfg.backupSenderCfg.queue = server.queue;
-var backupSender = new BackupSender(cfg.backupSenderCfg);
+_config.backupSenderCfg.queue = server.queue;
+var backupSender = new BackupSender(_config.backupSenderCfg);
 
 server.init();
 
