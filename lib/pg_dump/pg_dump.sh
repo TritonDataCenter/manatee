fd4aadc082dc4385ccf004d05d7fcaa0304cb85e
Yunong Xiao <yunong@joyent.com>
2013-06-28 04:40:51 +0000

    MANATEE-116 cr feedback, use ctrun to spawn postgres

--- lib/pg_dump/pg_dump.sh
+++ lib/pg_dump/pg_dump.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 echo ""   # blank line in log file helps scroll btwn instances
+source /root/.bashrc # source in the manta configs such as the url and credentials
 export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 set -o xtrace
 set -o pipefail
 
 PATH=/opt/smartdc/manatee/build/node/bin:/opt/local/bin:/usr/sbin/:/usr/bin:/usr/sbin:/usr/bin:/opt/smartdc/registrar/build/node/bin:/opt/smartdc/registrar/node_modules/.bin:/opt/smartdc/manatee/lib/tools:/opt/smartdc/manatee/lib/pg_dump/
+
 CFG=/opt/smartdc/manatee/etc/backup.json
 ZFS_CFG=/opt/smartdc/manatee/etc/snapshotter.json
-
-# The 'm' commands will pick these up automagically if they are exported.
-export MANTA_URL=$(cat $CFG | json -a manta_url)
-export MANTA_USER="poseidon"
-export MANTA_KEY_ID=`ssh-keygen -lf ~/.ssh/id_rsa.pub | cut -d ' ' -f2`
-export MANTA_TLS_INSECURE=$(cat $CFG | json -a manta_tls_insecure)
-MANATEE_STAT=/opt/smartdc/manatee/bin/manatee-stat
-DATASET=$(cat $ZFS_CFG | json dataset)
-DUMP_DATASET=zones/$(zonename)/data/pg_dump
-PG_DIR=/$DUMP_DATASET/data
-UPLOAD_SNAPSHOT=$(cat $CFG | json -a upload_snapshot)
+PG_PID=
 
 function fatal
 {
     echo "$(basename $0): fatal error: $*"
-    pkill -9 -f "postgres -D $PG_DIR"
+    kill -9 $PG_PID
     zfs destroy -R $DUMP_DATASET
     exit 1
 }
 
+MANATEE_STAT=/opt/smartdc/manatee/bin/manatee-stat
+DATASET=$(cat $ZFS_CFG | json dataset)
+[[ -n "$DATASET" ]] || fatal "unable to retrieve DATASET"
+DUMP_DATASET=zones/$(zonename)/data/pg_dump
+PG_DIR=/$DUMP_DATASET/data
+UPLOAD_SNAPSHOT=$(cat $CFG | json -a upload_snapshot)
 my_ip=$(mdata-get sdc:nics.0.ip)
-[[ $? -eq 0 ]] || fatal "Unable to retrieve our own IP address"
+[[ -n "$my_ip" ]] || fatal "Unable to retrieve our own IP address"
 svc_name=$(cat $CFG | json -a service_name)
-[[ $? -eq 0 ]] || fatal "Unable to retrieve service name"
+[[ -n "$svc_name" ]] || fatal "Unable to retrieve service name"
 zk_ip=$(cat $CFG | json -a zkCfg.servers.0.host)
-[[ $? -eq 0 ]] || fatal "Unable to retrieve nameservers from metadata"
+[[ -n "$zk_ip" ]] || fatal "Unable to retrieve nameservers from metadata"
 dump_dir=/var/tmp/upload
 mkdir $dump_dir
 
@@ -42,7 +40,9 @@ mput='/opt/smartdc/manatee/node_modules/manta/bin/mput'
 function upload_zfs_snapshot
 {
     echo "take a snapshot"
-    zfs snapshot $DATASET@$(date +%s)000
+    local zfs_snapshot=$DATASET@$(date +%s)000
+    zfs snapshot $zfs_snapshot
+    [[ $? -eq 0 ]] || fatal "Unable to create a snapshot"
     echo "getting latest snapshot"
     snapshot=$(zfs list -Hp -t snapshot | grep $DATASET | tail -n 1 | cut -f1)
     [[ $? -eq 0 ]] || fatal "Unable to retrieve latest snapshot"
@@ -50,18 +50,14 @@ function upload_zfs_snapshot
     # only upload the snapshot if the flag is set
     if [[ UPLOAD_SNAPSHOT -eq 1 ]]
     then
-        # column 4 is the refer size
-        local snapshot_size=$(zfs list -Hp -t snapshot | grep $DATASET | tail -n 1 | cut -f4)
+        local snapshot_size=$(zfs list -Hp -o refer -t snapshot $snapshot)
         [[ $? -eq 0 ]] || fatal "Unable to retrieve snapshot size"
         # pad the snapshot_size by 5% since there's some zfs overhead, note the
         # last bit just takes the floor of the floating point value
         local snapshot_size=$(echo "$snapshot_size * 1.05" | bc | cut -d '.' -f1)
+        [[ -n "$snapshot_size" ]] || fatal "Unable to calculate snapshot size"
         local manta_dir_prefix=/poseidon/stor/manatee_backups
-        local year=$(date -u +%Y)
-        local month=$(date -u +%m)
-        local day=$(date -u +%d)
-        local hour=$(date -u +%H)
-        local dir=$manta_dir_prefix/$svc_name/$year/$month/$day/$hour
+        local dir=$manta_dir_prefix/$svc_name/$(date -u +%Y/%m/%d/%H)
         $mmkdir -p -u $MANTA_URL -a $MANTA_USER -k $MANTA_KEY_ID $dir
         [[ $? -eq 0 ]] || fatal "unable to create backup dir"
         echo "sending snapshot $snapshot to manta"
@@ -86,38 +82,37 @@ function mount_data_set
     # remove postmaster.pid
     rm -f $PG_DIR/postmaster.pid
 
-    sudo -u postgres postgres -D $PG_DIR -p 23456 &
+    ctrun -o noorphan sudo -u postgres postgres -D $PG_DIR -p 23456 &
+    PG_PID=$!
     [[ $? -eq 0 ]] || fatal "unable to start postgres"
 
     echo 'sleep some seconds so we wait for pg to start'
-    sleep 20
+    sleep 3
     echo "postgres started"
 }
 
 function backup
 {
-    local year=$(date -u +%Y)
-    local month=$(date -u +%m)
-    local day=$(date -u +%d)
-    local hour=$(date -u +%H)
+    local date=$(date -u +%Y-%m-%d-%H)
 
     echo "getting db tables"
-    schema=$dump_dir/$year-$month-$day-$hour'_schema'
+    schema=$dump_dir/$date'_schema'
     # trim the first 3 lines of the schema dump
     sudo -u postgres psql -p 23456 moray -c '\dt' | sed -e '1,3d' > $schema
     [[ $? -eq 0 ]] || (rm $schema; fatal "unable to read db schema")
     for i in `sed 'N;$!P;$!D;$d' $schema | tr -d ' '| cut -d '|' -f2`
     do
         local time=$(date -u +%F-%H-%M-%S)
-        local dump_file=$dump_dir/$year-$month-$day-$hour'_'$i-$time.gz
+        local dump_file=$dump_dir/$date'_'$i-$time.gz
         sudo -u postgres pg_dump -p 23456 moray -a -t $i | sqlToJson.js | gzip -1 > $dump_file
         [[ $? -eq 0 ]] || (rm $schema; fatal "Unable to dump table $i")
     done
     # dump the entire moray db as well for manatee backups.
-    full_dump_file=$dump_dir/$year-$month-$day-$hour'_'moray-$time.gz
+    full_dump_file=$dump_dir/$date'_'moray-$time.gz
     sudo -u postgres pg_dump -p 23456 moray | gzip -1 > $full_dump_file
     [[ $? -eq 0 ]] || (rm $schema; fatal "Unable to dump full moray db")
     rm $schema
+    [[ $? -eq 0 ]] || fatal "unable to remove schema"
 }
 
 function upload
@@ -156,7 +151,7 @@ function upload
 
 function cleanup
 {
-    pkill -9 -f "postgres -D $PG_DIR"
+    kill -9 $PG_PID
     [[ $? -eq 0 ]] || fatal "unable to kill postgres"
     zfs destroy -R $DUMP_DATASET
     [[ $? -eq 0 ]] || fatal "unable destroy dataset"
@@ -168,14 +163,14 @@ read -r svc_name_delim< <(echo $svc_name | gsed -e 's|\.|\\.|g')
 
 # figure out if we are the peer that should perform backups.
 shard_info=$($MANATEE_STAT $zk_ip:2181 -s $svc_name)
-[[ $? -eq 0 ]] || fatal "Unable to retrieve shardinfo from zookeeper"
+[[ -n $shard_info ]] || fatal "Unable to retrieve shardinfo from zookeeper"
 
 async=$(echo $shard_info | json $svc_name_delim.async.ip)
-[[ $? -eq 0 ]] || fatal "unable to parse async peer"
+[[ -n $async ]] || echo "warning: unable to parse async peer"
 sync=$(echo $shard_info | json $svc_name_delim.sync.ip)
-[[ $? -eq 0 ]] || fatal "unable to parse sync peer"
+[[ -n $sync ]] || echo "warning: unable to parse sync peer"
 primary=$(echo $shard_info | json $svc_name_delim.primary.ip)
-[[ $? -eq 0 ]] || fatal "unable to parse primary peer"
+[[ -n $primary ]] || fatal "unable to parse primary peer"
 
 continue_backup=0
 if [ "$async" = "$my_ip" ]
@@ -198,6 +193,8 @@ else
     fi
 fi
 
+#XXX remove this
+continue_backup = 1
 if [ $continue_backup = '1' ]
 then
     upload_zfs_snapshot
