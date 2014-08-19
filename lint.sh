94708e00bb6a730a2e381c7606af07f8374b8435
Yunong Xiao <yunong@joyent.com>
2014-08-19 22:49:13 +0000

    MANATEE-219 lint clean

--- lint.sh
+++ lint.sh
@@ -11,7 +11,7 @@ cd ../..
 ./deps/javascriptlint/build/install/jsl --conf ./tools/jsl.node.conf \
     ./*.js ./lib/*.js ./test/*.js ./bin/manatee-adm
 
-./deps/jsstyle/jsstyle -f ./tools/jsstyle.conf ./*.js  \
+./deps/jsstyle/jsstyle -f ./tools/jsstyle.conf ./*.js  ./lib/*.js \
     ./test/*.js ./bin/manatee-adm
 
 ./deps/json/lib/jsontool.js -f ./package.json 1>/dev/null
